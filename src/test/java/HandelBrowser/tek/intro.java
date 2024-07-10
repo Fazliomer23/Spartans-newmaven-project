5e2756e3b76b5f02c004359c8d3cfa45c883477c
fazliomerali <fazliomerkhan@gmail.com>
2024-07-09 21:46:40 -0400

    my new selenium oriject

--- src/test/java/HandelBrowser/tek/intro.java
+++ src/test/java/HandelBrowser/tek/intro.java
@@ -7,7 +7,7 @@ public class intro {
         ChromeDriver driver = new ChromeDriver();
         driver.get("https://google.com");
         driver.getTitle();
-        driver.get("https://facebook.com");
+        driver.get("https://dev.insurance.tekschool-students.com/");
         String title =driver.getTitle();
         System.out.println(title);
 
