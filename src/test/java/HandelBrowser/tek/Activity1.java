c728e83b360c1ed1a9e491d42e94fa61ef171554
fazliomerali <fazliomerkhan@gmail.com>
2024-07-09 21:51:29 -0400

    my new selenium oriject

---
+++ src/test/java/HandelBrowser/tek/Activity1.java
@@ -0,0 +1,15 @@
+package HandelBrowser.tek;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Activity1 {
+    public static void main(String[] args) {
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();;
+        driver.get("https://dev.insurance.tekschool-students.com");
+        String title = driver.getTitle();
+        System.out.println(title);
+        driver.quit();
+
+    }
+}
