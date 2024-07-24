d6480876fbfacc0ab37e07d264a103f70f18545b
fazliomerali <fazliomerkhan@gmail.com>
2024-07-23 21:23:13 -0400

    My Activity

---
+++ src/test/java/table/TablActivity.java
@@ -0,0 +1,30 @@
+package table;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class TablActivity {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://dev.insurance.tekschool-students.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+        driver.findElement(By.linkText("Login")).click();
+        driver.findElement(By.name("username")).sendKeys("supervisor");
+        driver.findElement(By.name("password")).sendKeys("tek_supervisor");
+        driver.findElement(By.xpath("//button[text()='Sign In']")).click();
+
+        driver.findElement(By.linkText("Plans")).click();
+
+        WebElement priceElement = driver.findElement(By.xpath("//table//tr[1]//td[contains(@class, 'price')]"));
+        String priceValue = priceElement.getText();
+        System.out.println("Price Value: " + priceValue);
+
+        driver.quit();
+    }
+}
